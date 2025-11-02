1c2072287a6892b67b9161650e7aaf6468f646ea
Trần Đức Thái <tranducthai8@gmail.com>
2025-11-02 18:04:26 +0700

    port mapping to env

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
